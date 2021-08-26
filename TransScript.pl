e80faa680cce833c8e8f8ebc88c2c90a4b144803
pulish-xg <89562847+pulish-xg@users.noreply.github.com>
2021-08-26 14:00:45 +0900

    add TransScript.pl (empty)

---
+++ TransScript.pl
@@ -0,0 +1,12 @@
+#!C:\Strawberry\perl\bin\perl.exe
+use v5.10; # state
+use strict;
+use warnings;
+
+use Win32;
+use Win32::Process;
+use Win32::Process::Info; # cpanm install Win32::Process::Info
+use Win32::Process::Memory; # cpanm install Win32::Process::Memory
+use Win32::Process::List; # cpanm install Win32::Process::List
+use File::Monitor; # cpanm install File::Monitor
+
